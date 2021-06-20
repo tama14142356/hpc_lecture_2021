4be64d4387d871ba6c786e28d312cb2305617d2b
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2021-06-20 21:02:06 +0900

    Hacking CUTLASS Step 75.

--- 08_cache_gpu/05_cutlass.cu
+++ 08_cache_gpu/05_cutlass.cu
@@ -6,18 +6,6 @@
 #include <chrono>
 using namespace std;
 
-inline __device__
-void store(float *ptr, const float &src) {
-  asm volatile ("st.global.cg.f32 [%0], %1;\n"
-		: : "l"(ptr), "f"(src));
-}
-
-inline __device__
-static void gemm(const float &a, const float &b, float &c) {
-  asm volatile ( "fma.rn.f32 %0, %1, %2, %3;\n"
-		 : "=f"(c) : "f"(a), "f"(b), "f"(c));
-}
-
 __global__ void kernel(int dim_m, int dim_n, int dim_k,
 		       float *d_a, float *d_b, float *d_c) {
   const int ItemsPerVector = 4;
@@ -64,7 +52,7 @@ __global__ void kernel(int dim_m, int dim_n, int dim_k,
   tile_b = reinterpret_cast<vec_t*>(&d_b[((b_n + offset_b_n) * ldb + b_k) * ItemsPerVector]);
   for (int m = 0; m < ItemsPerThread; ++m)
     for (int n = 0; n < ItemsPerThread; ++n)
-      fragment_c[m][n] = float(0);
+      fragment_c[m][n] = 0;
 
   int warp_id = threadIdx.x / ThreadsPerWarp; // 2
   int warp_x = warp_id % WarpsPerBlockX; // 2
@@ -101,7 +89,7 @@ __global__ void kernel(int dim_m, int dim_n, int dim_k,
       }
       for (int m = 0; m < ItemsPerThread; ++m) {
 	for (int n = 0; n < ItemsPerThread; ++n) {
-	  gemm(fragment_a[m], fragment_b[n], fragment_c[m][n]);
+	  fragment_c[m][n] += fragment_a[m] * fragment_b[n];
 	}
       }
     }
@@ -116,7 +104,7 @@ __global__ void kernel(int dim_m, int dim_n, int dim_k,
       int by = ItemsPerBlockX * blockIdx.x + ty;
       for (int i = 0; i < ItemsPerVector; ++i) {
 	if (bx < dim_n && (by + i) < dim_m) {
-	  store(d_c + bx * dim_m + by + i, fragment_c[iy + i][ix]);
+	  d_c[bx * dim_m + by + i] = fragment_c[iy + i][ix];
 	}
       }
     }
