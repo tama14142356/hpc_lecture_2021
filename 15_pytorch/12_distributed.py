c30958ac6816dc06639ca88513b972af67b74b79
tomo <tomo14142356@gmail.com>
2021-07-15 15:19:18 +0900

    Add all gather code

--- 15_pytorch/12_distributed.py
+++ 15_pytorch/12_distributed.py
@@ -8,12 +8,17 @@ method = "tcp://{}:{}".format(master_addr, master_port)
 rank = int(os.getenv('OMPI_COMM_WORLD_RANK', '0'))
 world_size = int(os.getenv('OMPI_COMM_WORLD_SIZE', '1'))
 dist.init_process_group("nccl", init_method=method, rank=rank, world_size=world_size)
-print('Rank: {}, Size: {}, Host: {}'.format(dist.get_rank(), dist.get_world_size(), master_addr))
+print('Rank: {}, Size: {}, Host: {}'.format(dist.get_rank(), dist.get_world_size(),
+                                            master_addr))
 
 ngpus = torch.cuda.device_count()
 device = rank % ngpus
 x = torch.randn(1).to(device)
+output = [torch.ones(1, ).to(device) for _ in range(world_size)]
 print('rank {}: {}'.format(rank, x))
-dist.broadcast(x, src=0)
+print('rank {}: {}'.format(rank, output))
+# dist.broadcast(x, src=0)
+dist.all_gather(output, x)
 print('rank {}: {}'.format(rank, x))
+print('rank {}: {}'.format(rank, output))
 dist.destroy_process_group()
