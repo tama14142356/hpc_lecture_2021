bfba9e3b7336bd8de1c5697d6edd051e6b8725a5
tomo <tomo14142356@gmail.com>
2021-08-11 00:56:00 +0900

    Add calc mean

--- 15_pytorch/12_distributed.py
+++ 15_pytorch/12_distributed.py
@@ -22,10 +22,13 @@ print('rank {}: {}'.format(rank, output))
 # dist.all_gather(output, x)
 dist.all_reduce(x, op=dist.ReduceOp.SUM)
 y = torch.sum(x)
+nb = x.shape[0] * dist.get_world_size()
+mean = y / nb
 # nccl not supported gather func
 # if rank == 0:
 #     dist.gather(x, gather_list=output, dst=0)
 print('rank {}: {}'.format(rank, x))
 print('rank {}: {}'.format(rank, y))
+print('rank {}: {}'.format(rank, mean))
 print('rank {}: {}'.format(rank, output))
 dist.destroy_process_group()
