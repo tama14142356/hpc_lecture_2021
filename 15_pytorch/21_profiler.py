bc34d65b0ebac4e9a35bbc80b53237c14bd301d6
tomo <tomo14142356@gmail.com>
2021-09-25 11:58:18 +0900

    Add profile for ddp, job script

--- 15_pytorch/21_profiler.py
+++ 15_pytorch/21_profiler.py
@@ -1,10 +1,24 @@
+import argparse
+import os
+
 import torch
+import torch.distributed as dist
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.profiler
 from torchvision import datasets, transforms
-from torch.utils.tensorboard import SummaryWriter
+# from torch.utils.tensorboard import SummaryWriter
 import time
+
+
+def print0(message):
+    if dist.is_initialized():
+        if dist.get_rank() == 0:
+            print(message, flush=True)
+            # print("init", flush=True)
+    else:
+        print(message, flush=True)
 
 
 class CNN(nn.Module):
@@ -33,15 +47,16 @@ class CNN(nn.Module):
         return output
 
 
-def train(train_loader, model, criterion, optimizer, epoch, device):
+def train(train_loader, model, criterion, optimizer, epoch, device, logs):
     model.train()
     t = time.perf_counter()
     with torch.profiler.profile(
             schedule=torch.profiler.schedule(wait=1, warmup=1, active=8),
-            on_trace_ready=torch.profiler.tensorboard_trace_handler("./log"),
-            record_shapes=True) as prof:
+            on_trace_ready=torch.profiler.tensorboard_trace_handler(logs),
+            record_shapes=True,
+            profile_memory=False) as prof:
         for batch_idx, (data, target) in enumerate(train_loader):
-            print("batch idx: ", batch_idx)
+            print0("batch idx: {}".format(batch_idx))
             data = data.to(device)
             target = target.to(device)
             output = model(data)
@@ -50,10 +65,10 @@ def train(train_loader, model, criterion, optimizer, epoch, device):
             loss.backward()
             optimizer.step()
             prof.step()
-            print('Train Epoch: {} [{:>5}/{} ({:.0%})]\tLoss: {:.6f}\t Time:{:.4f}'.
-                  format(epoch, batch_idx * len(data), len(train_loader.dataset),
-                         batch_idx / len(train_loader), loss.data.item(),
-                         time.perf_counter() - t))
+            print0('Train Epoch: {} [{:>5}/{} ({:.0%})]\tLoss: {:.6f}\t Time:{:.4f}'.
+                   format(epoch, batch_idx * len(data), len(train_loader.dataset),
+                          batch_idx / len(train_loader), loss.data.item(),
+                          time.perf_counter() - t))
             t = time.perf_counter()
             if batch_idx > 10:
                 break
@@ -73,36 +88,77 @@ def validate(val_loader, model, criterion, device):
 
     val_loss /= len(val_loader)
     val_acc /= len(val_loader)
-    print('\nValidation set: Average loss: {:.4f}, Accuracy: {:.1f}%\n'.format(
+    print0('\nValidation set: Average loss: {:.4f}, Accuracy: {:.1f}%\n'.format(
         val_loss, val_acc))
 
 
-def main():
-    epochs = 1
-    batch_size = 32
-    learning_rate = 1.0e-02
+def main(args):
+    master_addr = os.getenv("MASTER_ADDR", default="localhost")
+    master_port = os.getenv('MASTER_PORT', default='8888')
+    method = "tcp://{}:{}".format(master_addr, master_port)
+    rank = int(os.getenv('OMPI_COMM_WORLD_RANK', '0'))
+    world_size = int(os.getenv('OMPI_COMM_WORLD_SIZE', '1'))
+    dist.init_process_group("nccl",
+                            init_method=method,
+                            rank=rank,
+                            world_size=world_size)
+    ngpus = torch.cuda.device_count()
+    device = torch.device('cuda', rank % ngpus)
+
+    epochs = args.epochs
+    batch_size = args.batch_size
+    learning_rate = args.learning_rate
 
     train_dataset = datasets.MNIST('./data',
                                    train=True,
                                    download=True,
                                    transform=transforms.ToTensor())
     val_dataset = datasets.MNIST('./data', train=False, transform=transforms.ToTensor())
+
+    train_sampler = torch.utils.data.distributed.DistributedSampler(
+        train_dataset, num_replicas=dist.get_world_size(), rank=dist.get_rank())
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                                batch_size=batch_size,
-                                               shuffle=True)
+                                               sampler=train_sampler)
+    # shuffle=True)
+
+    val_sampler = torch.utils.data.distributed.DistributedSampler(
+        val_dataset, num_replicas=dist.get_world_size(), rank=dist.get_rank())
     val_loader = torch.utils.data.DataLoader(dataset=val_dataset,
                                              batch_size=batch_size,
-                                             shuffle=False)
-    device = torch.device("cuda")
+                                             sampler=val_sampler)
+    # shuffle=False)
     model = CNN().to(device)
+    model = DDP(model, device_ids=[rank % ngpus])
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
     for epoch in range(epochs):
         model.train()
-        train(train_loader, model, criterion, optimizer, epoch, device)
+        train(train_loader, model, criterion, optimizer, epoch, device, args.logs)
         validate(val_loader, model, criterion, device)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='PyTorch MNIST profile Example')
+    parser.add_argument('--logs',
+                        type=str,
+                        default="./logs",
+                        help='profile log directory (default: ./logs)')
+    parser.add_argument('--batch_size',
+                        type=int,
+                        default=32,
+                        metavar='N',
+                        help='input batch size for training (default: 32)')
+    parser.add_argument('--epochs',
+                        type=int,
+                        default=1,
+                        metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--learning_rate',
+                        type=float,
+                        default=1.0e-02,
+                        metavar='LR',
+                        help='learning rate (default: 1.0e-02)')
+    args = parser.parse_args()
+    main(args)
--- 15_pytorch/jobs/master_address.sh
+++ 15_pytorch/jobs/master_address.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 #$ -cwd
 #$ -l rt_G.large=1
-#$ -l h_rt=0:10:00
-#$ -N ddp
+#$ -l h_rt=2:10:00
+#$ -N profile
 #$ -j y
+
+START_TIMESTAMP=$(date '+%s')
 
 # MASTER_ADDR=$(/usr/sbin/ip a show | grep inet | grep 192.168.205 | head -1 | cut -d " " -f 6 | cut -d "/" -f 1)
 MASTER_ADDR=$(/usr/sbin/ip a show dev bond0 | grep 'inet ' | cut -d " " -f 6 | cut -d "/" -f 1)
 MASTER_PORT=$((10000 + ($JOB_ID % 50000)))
 # MASTER_PORT=$((10000 + ($SLURM_JOBID % 50000)))
+
+nodes=$NHOSTS
+gpus_pernode=4
+gpus=$(($nodes * $gpus_pernode))
 
 . /etc/profile.d/modules.sh
 
@@ -20,9 +26,16 @@ module load nccl/2.7/2.7.8-1
 module load openmpi
 
 source ~/.bashrc
+date_str="$(date '+%Y%m%d_%H%M%S')"
 
 mpirun \
-    -np 4 \
+    -np $gpus \
+    -npernode $gpus_pernode \
     -x MASTER_ADDR=$MASTER_ADDR \
     -x MASTER_PORT=$MASTER_PORT \
-    python 13_ddp.py
+    python 21_profiler.py --logs "runs/logs/$date_str"
+
+END_TIMESTAMP=$(date '+%s')
+
+E_TIME=$(($END_TIMESTAMP-$START_TIMESTAMP))
+echo $E_TIME
