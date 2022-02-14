b3f865e0efee63c639b6bd5e4d49b42ad493a9ff
tomo <tomo14142356@gmail.com>
2022-02-14 16:26:22 +0900

    Add shard tar process, subset option

--- 15_pytorch/webdataset_archive.py
+++ 15_pytorch/webdataset_archive.py
@@ -24,8 +24,11 @@ def create_pickle_webdataset(dataset, name, is_test=False):
     sink.close()
 
 
-def create_img_webdataset(dataset, name, is_test=False):
-    sink = wds.TarWriter("{}.tar".format(name))
+def create_img_webdataset(dataset, subset, name, is_test=False):
+    length = len(dataset)
+    num_tars = 4
+    tar_files = [f"{name}-{subset}-{i:03d}.tar" for i in range(num_tars)]
+    sinks = [wds.TarWriter(tar_file) for tar_file in tar_files]
     for index, data in enumerate(dataset):
         if is_test:
             input, output = data
@@ -33,12 +36,13 @@ def create_img_webdataset(dataset, name, is_test=False):
             input, output = data, 1
         if index % 1000 == 0:
             print(f"{index:6d}", end="\r", flush=True, file=sys.stderr)
-        sink.write({
+        sinks[index % num_tars].write({
             "__key__": "%06d" % index,
             "jpg": input,
             "cls": output,
         })
-    sink.close()
+    for sink in sinks:
+        sink.close()
 
 
 if __name__ == "__main__":
@@ -49,12 +53,14 @@ if __name__ == "__main__":
                         default="img")
     parser.add_argument("--path", type=str, default="./data")
     parser.add_argument("--name", type=str, default="mnist")
+    parser.add_argument("--subset", type=str, default="train")
     parser.add_argument("--datasetname", type=str, default="mnist")
     args = parser.parse_args()
 
     is_test = False
+    is_train = args.subset == "train"
     if args.datasetname == "mnist":
-        dataset = torchvision.datasets.MNIST(root=args.path, download=True)
+        dataset = torchvision.datasets.MNIST(root=args.path, train=is_train, download=True)
         is_test = True
     # elif args.datasetname == "bdd":
     #     dataset = BDD(args.path)
@@ -62,4 +68,4 @@ if __name__ == "__main__":
     if args.type == "pickle":
         create_pickle_webdataset(dataset, args.name, is_test)
     elif args.type == "img":
-        create_img_webdataset(dataset, args.name, is_test)
+        create_img_webdataset(dataset, args.subset, args.name, is_test)
