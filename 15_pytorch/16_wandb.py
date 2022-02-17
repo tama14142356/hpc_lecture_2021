335300db7fb7b44c29f81c2b85727219108cae40
tomo <tomo14142356@gmail.com>
2022-02-17 12:41:19 +0900

    Fix wandb log

--- 15_pytorch/16_wandb.py
+++ 15_pytorch/16_wandb.py
@@ -1,14 +1,16 @@
 import argparse
+from collections import defaultdict
+import glob
+import os
+import time
+
 import torch
 import torch.distributed as dist
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import datasets, transforms
 from torch.nn.parallel import DistributedDataParallel as DDP
-import time
 import wandb
-import os
-import glob
 import webdataset as wds
 
 from utils import dist_setup, dist_cleanup, myget_rank_size
@@ -95,19 +97,23 @@ class ProgressMeter(object):
 
 
 def train(train_loader, model, criterion, optimizer, epoch, device, length):
+    wandb_log_output = defaultdict(list)
     batch_time = AverageMeter('Time', ':.4f')
     train_loss = AverageMeter('Loss', ':.6f')
     train_acc = AverageMeter('Accuracy', ':.6f')
     progress = ProgressMeter(length, [train_loss, train_acc, batch_time],
                              prefix="Epoch: [{}]".format(epoch))
+    print_rank("start train")
+    rank, _ = myget_rank_size()
     model.train()
-    t = time.perf_counter()
+    t1 = time.perf_counter()
+    t2 = t1
     for batch_idx, (data, target) in enumerate(train_loader):
         data = data.to(device)
         target = target.to(device)
-        print_rank(batch_idx, target.size(), data.size())
+        # print_rank(batch_idx, target.size(), data.size())
         output = model(data)
-        print_rank(batch_idx, output.size(), data.size())
+        # print_rank(batch_idx, output.size(), data.size())
         loss = criterion(output, target)
         train_loss.update(loss.item(), data.size(0))
         pred = output.data.max(1)[1]
@@ -117,24 +123,32 @@ def train(train_loader, model, criterion, optimizer, epoch, device, length):
         loss.backward()
         optimizer.step()
         if batch_idx % 200 == 0:
-            cur_time = time.perf_counter() - t
+            cur_time = time.perf_counter() - t1
             batch_time.update(cur_time)
-            t = time.perf_counter()
+            t1 = time.perf_counter()
             progress.display(batch_idx)
-            # rank = 0
-            # if dist.is_initialized():
-            #     rank = dist.get_rank()
-            # if rank == 0:
-            #     wandb.log({"sec/batch": cur_time})
-    return train_loss.avg, train_acc.avg, batch_time.avg
+        if rank == 0:
+            cur_time = time.perf_counter() - t2
+            t2 = time.perf_counter()
+            # wandb.log({
+            #     "loss/train": loss.item(),
+            #     "acc/train": acc,
+            #     "sec/batch": cur_time}, commit=False)
+            wandb_log_output["loss/train"].append(loss.item())
+            wandb_log_output["acc/train"].append(acc)
+            wandb_log_output["sec/batch"].append(cur_time)
+    return train_loss.avg, train_acc.avg, batch_time.avg, wandb_log_output
 
 
 def validate(val_loader, model, criterion, device, length):
+    wandb_log_output = defaultdict(list)
     val_loss = AverageMeter('Loss', ':.6f')
     val_acc = AverageMeter('Accuracy', ':.1f')
     progress = ProgressMeter(length, [val_loss, val_acc],
                              prefix='\nValidation: ',
                              postfix='\n')
+    print_rank("start val")
+    rank, _ = myget_rank_size()
     model.eval()
     for data, target in val_loader:
         data = data.to(device)
@@ -145,11 +159,19 @@ def validate(val_loader, model, criterion, device, length):
         pred = output.data.max(1)[1]
         acc = 100. * pred.eq(target.data).cpu().sum() / target.size(0)
         val_acc.update(acc, data.size(0))
+        if rank == 0:
+            # wandb.log({
+            #     "loss/val": loss.item(),
+            #     "acc/val": acc}, commit=False)
+            wandb_log_output["loss/val"].append(loss.item())
+            wandb_log_output["acc/val"].append(acc)
+
     progress.display(length)
-    return val_loss.avg, val_acc.avg
+    return val_loss.avg, val_acc.avg, wandb_log_output
 
 
 def main():
+    print_rank("start main")
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
     parser.add_argument('--bs',
                         '--batch_size',
@@ -172,10 +194,15 @@ def main():
                         type=str,
                         default="nccl",
                         choices=["nccl", "mpi", "gloo"])
+    parser.add_argument('--data_path',
+                        type=str,
+                        default="./data/webdatasets/mnist/16_datas",
+                        help='dataset path')
     parser.add_argument("--use_wds", action="store_true")
     args = parser.parse_args()
 
     dist_setup(backend=args.mpi_backend)
+    print_rank("dist setup complete")
     rank, world_size = myget_rank_size()
     # is_cuda_avail = torch.cuda.is_available()
     if args.mpi_backend == "nccl":
@@ -187,6 +214,7 @@ def main():
     if rank == 0:
         wandb.init(project="ssl_test_result", entity="tomo", name="wandb_mnist_tmp")
         wandb.config.update(args)
+    print_rank("wandb setup complete")
 
     if args.use_wds:
         # normalize = transforms.Normalize((0.1307, ), (0.3081, ))
@@ -200,14 +228,13 @@ def main():
 
         train_img_num = 60000
         train_length = train_img_num // args.bs
-        train_tars = sorted(
-            glob.glob(os.path.join("./data/webdatasets", "train", "*.tar")))
+        train_tars = sorted(glob.glob(os.path.join(args.data_path, "train", "*.tar")))
         train_dataset = wds.WebDataset(train_tars).decode("pill").to_tuple("jpg", "cls")
         train_dataset = train_dataset.map_tuple(preproc, lambda x: torch.tensor(x))
 
         val_img_num = 10000
         val_length = val_img_num // args.bs
-        val_tars = sorted(glob.glob(os.path.join("./data/webdatasets", "val", "*.tar")))
+        val_tars = sorted(glob.glob(os.path.join(args.data_path, "val", "*.tar")))
         val_dataset = wds.WebDataset(val_tars).decode("pill").to_tuple("jpg", "cls")
         val_dataset = val_dataset.map_tuple(preproc, lambda x: torch.tensor(x))
 
@@ -236,6 +263,8 @@ def main():
                                                  shuffle=False)
         train_length, val_length = len(train_loader), len(val_loader)
 
+    print_rank("load setup complete")
+
     model = CNN().to(device)
     if rank == 0:
         wandb.config.update({"model": model.__class__.__name__, "dataset": "MNIST"})
@@ -246,26 +275,44 @@ def main():
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=args.lr)
 
+    print_rank("model setup complete")
+
     # data_plot = defaultdict(list)
     for epoch in range(args.epochs):
         model.train()
-        train_loss, train_acc, batch_time = train(train_loader, model, criterion,
-                                                  optimizer, epoch, device,
-                                                  train_length)
-        val_loss, val_acc = validate(val_loader, model, criterion, device, val_length)
+        train_loss, train_acc, batch_time, train_wandb_log = train(
+            train_loader, model, criterion, optimizer, epoch, device, train_length)
+        val_loss, val_acc, val_wandb_log = validate(val_loader, model, criterion,
+                                                    device, val_length)
         if rank == 0:
             # data_plot["train_loss"].append([epoch, train_loss])
             # data_plot["train_acc"].append([epoch, train_acc])
             # data_plot["val_loss"].append([epoch, val_loss])
             # data_plot["val_acc"].append([epoch, val_acc])
             # data_plot["batch_time"].append([epoch, val_acc])
-            wandb.log({
-                'train_loss': train_loss,
-                'train_acc': train_acc,
-                'val_loss': val_loss,
-                'val_acc': val_acc,
-                'batch_time': batch_time
-            })
+            train_keys = list(train_wandb_log.keys())
+            train_len = len(train_wandb_log[train_keys[0]])
+            val_keys = list(val_wandb_log.keys())
+            val_len = len(train_wandb_log[val_keys[0]])
+            max_len = max(train_len, val_len)
+
+            for i in range(max_len):
+                wandb_dict= {}
+                for k in train_keys:
+                    if i < train_len:
+                        wandb_dict[k] = train_wandb_log[k][i]
+                for k in val_keys:
+                    if i < val_len:
+                        wandb_dict[k] = val_wandb_log[k][i]
+                wandb.log(wandb_dict)
+
+            # wandb.log({
+            #     'avg/train_loss': train_loss,
+            #     'avg/train_acc': train_acc,
+            #     'avg/val_loss': val_loss,
+            #     'avg/val_acc': val_acc,
+            #     'avg/batch_time': batch_time
+            # })
     # if rank == 0:
     #     for key in data_plot:
     #         tmp_data = data_plot[key]
@@ -277,4 +324,5 @@ def main():
 
 
 if __name__ == '__main__':
+    print_rank("start program")
     main()
